660a60af7357bc20edebc3e75d57437c9f2afe3d
MirkaanStefanov <135588213+MirkaaStefanov@users.noreply.github.com>
2024-12-17 23:50:13 +0200

    security done

--- src/main/java/com/example/MafiaGame_api/config/SecurityConfiguration.java
+++ src/main/java/com/example/MafiaGame_api/config/SecurityConfiguration.java
@@ -32,17 +32,7 @@ public class SecurityConfiguration {
     private final AuthenticationProvider authenticationProvider;
     private final LogoutHandler logoutHandler;
     private final ObjectMapper objectMapper;
-    public static final String CLIENT_URL = "/api/v1/client/**";
-    public static final String PACKAGE_URL = "/api/v1/package/**";
-    public static final String PLATE_URL = "/api/v1/plate/**";
-    public static final String CARTON_URL = "/api/v1/carton/**";
-    public static final String PRODUCT_URL = "/api/v1/product/**";
-    public static final String ORDER_URL = "/api/v1/order/**";
-    public static final String ORDER_PRODUCT_URL = "/api/v1/orderProduct/**";
-    public static final String MONTHLY_ORDER_URL = "/api/v1/monthlyOrder/**";
-    public static final String MONTHLY_ORDER_PRODUCT_URL = "/api/v1/monthlyOrderProduct/**";
-    public static final String INVOICE_URL = "/api/v1/monthlyOrderProduct/**";
-    public static final String CLIENT_USER_URL = "/api/v1/client-user/**";
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -55,18 +45,8 @@ public class SecurityConfiguration {
                 .authenticationEntryPoint(new JwtAuthenticationEntryPoint(objectMapper))
                 .and()
                 .authorizeHttpRequests()
-                .requestMatchers(
-                        "/api/v1/images/uploadImageForPackage",
-                        "/api/v1/images/**",
-                        "/api/v1/auth/**",
-                        "/api/v1/user/forgot-password",
-                        "/api/v1/user/reset-password")
-                .permitAll()
-                .requestMatchers(HttpMethod.GET,"/api/v1/user/me").authenticated()
-                .requestMatchers(HttpMethod.GET,"/api/v1/user/{id}").authenticated()
-                .requestMatchers(HttpMethod.PUT,"/api/v1/user/authenticated/{id}").authenticated()
-                .requestMatchers(HttpMethod.GET,"/api/v1/user/ifPassMatch").authenticated()
-                .requestMatchers(HttpMethod.PUT,"/api/v1/user/change-pass").authenticated()
+
+
 
                 .anyRequest()
                 .permitAll()
@@ -77,7 +57,7 @@ public class SecurityConfiguration {
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .logout()
-                .logoutUrl("/api/v1/auth/logout")
+                .logoutUrl("/auth/logout")
                 .addLogoutHandler(logoutHandler)
                 .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext());
 
