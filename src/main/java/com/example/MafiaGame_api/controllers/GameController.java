bd9c442c8ba689b8e556ea111eb75e24c883315b
MirkaanStefanov <135588213+MirkaaStefanov@users.noreply.github.com>
2024-12-23 01:15:46 +0200

    create game controller

--- src/main/java/com/example/MafiaGame_api/controllers/GameController.java
+++ src/main/java/com/example/MafiaGame_api/controllers/GameController.java
@@ -5,6 +5,7 @@ import com.example.MafiaGame_api.services.GameService;
 import lombok.AllArgsConstructor;
 import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,15 +18,19 @@ public class GameController {
 
     private final GameService gameService;
 
+    @PostMapping("/create")
     public ResponseEntity<GameDTO> createGame(@RequestHeader("Authorization") String auth) {
         return ResponseEntity.ok(gameService.createGame());
     }
 
+    @PostMapping("/enter")
     public ResponseEntity<GameDTO> enterGame(@RequestParam Long id, @RequestHeader("Authorization") String auth) throws ChangeSetPersister.NotFoundException {
         return ResponseEntity.ok(gameService.enterGame(id));
     }
 
+    @PostMapping("/start")
     public void startGame(@RequestParam Long gameId, @RequestParam int killerQuantity, @RequestParam int doctorQuantity, @RequestParam int policeQuantity, @RequestHeader("Authorization") String auth) throws ChangeSetPersister.NotFoundException {
         gameService.startGame(gameId, killerQuantity, doctorQuantity, policeQuantity);
     }
+
 }
